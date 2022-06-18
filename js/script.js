2aa7f2474824aa33146ecb14563c8cee4b40082d
Rafael <rfaelnc@gmail.com>
2022-06-17 23:55:21 -0300

    add function start

--- js/script.js
+++ js/script.js
@@ -1,33 +1,53 @@
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
 
-const jump = () => {
-  mario.classList.add("jump-mario");
+function initGame() {
+  const jump = () => {
+    mario.classList.add("jump-mario");
 
-  setTimeout(() => {
-    mario.classList.remove("jump-mario");
-  }, 500);
-};
+    setTimeout(() => {
+      mario.classList.remove("jump-mario");
+    }, 500);
+  };
 
-const loopGame = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window
-    .getComputedStyle(mario)
-    .bottom.replace("px", "");
+  const loopGame = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window
+      .getComputedStyle(mario)
+      .bottom.replace("px", "");
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+      pipe.style.animation = "none";
+      pipe.style.left = `${pipePosition}px`;
 
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
+      mario.style.animation = "none";
+      mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = "./Images/mario-game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "45px";
+      mario.src = "./Images/mario-game-over.png";
+      mario.style.width = "75px";
+      mario.style.marginLeft = "45px";
 
-    clearInterval(loopGame);
-  }
-}, 10);
+      clearInterval(loopGame);
+    }
+  }, 10);
 
-document.addEventListener("keydown", jump);
+  document.addEventListener("keydown", jump);
+}
+
+const button = document.querySelector(".menu-game button");
+
+button.addEventListener("click", startGame);
+
+pipe.style.animation = "none";
+pipe.style.left = `${pipePosition}px`;
+
+mario.style.animation = "none";
+mario.style.bottom = `${marioPosition}px`;
+
+function startGame() {
+  pipe.removeAttribute("style");
+  mario.removeAttribute("style");
+  mario.src = "./Images/super-mario.gif";
+
+  initGame();
+}
